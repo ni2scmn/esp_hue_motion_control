83d6f44b0fb4bbb301687a1cc89736bcaf9521ed
Schnakenberg, Nils <52465077+ni2scmn@users.noreply.github.com>
2025-02-16 15:27:39 +0100

    feat: impl app_status in config server to get and set with /status endpoint

--- components/config_server/include/config_server.h
+++ components/config_server/include/config_server.h
@@ -3,7 +3,17 @@
 #include "esp_err.h"
 #include "esp_http_server.h"
 
-httpd_handle_t create_config_server_handle(void);
+// static configuration struct
+typedef struct {
+  bool active;
+} app_config_t;
 
+static app_config_t config = {
+    .active = true,
+};
+
+const app_config_t *get_config();
+
+httpd_handle_t create_config_server_handle(void);
 esp_err_t start_config_server(httpd_handle_t *server);
 esp_err_t stop_config_server(httpd_handle_t *server);
